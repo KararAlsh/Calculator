95e2d7cee0143eceee735c8a20f4c5b24d42dbf9
KararAlsh <kararabdulsahib@hotmail.com>
2023-01-06 16:20:52 +0100

    Updated calculator js due to small mistake in code that wouldn let calculator answer

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@
 		 } else if (item.id == 'backspace') {
 			 let string = display.innerText.toString();
 			 display.innerText = string.substr(0, string.length - 1);
-		 } else if (display.innerText != '' && item.id == '') {
+		 } else if (display.innerText != '' && item.id == 'equal') {
 			 display.innerText = eval(display.innerText);
 		 } else if (display.innerText == '' && item.id == 'equal') {
 			 display.innerText(() => (display.innerText = ''), 2000);
